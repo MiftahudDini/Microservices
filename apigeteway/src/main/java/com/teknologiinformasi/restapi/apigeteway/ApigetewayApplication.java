fdadff67d05b0906939718ab9feb55ebbbbd045f
MiftahudDini <miftahutdini@gmail.com>
2025-03-12 11:02:24 +0700

    first commit

---
+++ apigeteway/src/main/java/com/teknologiinformasi/restapi/apigeteway/ApigetewayApplication.java
@@ -0,0 +1,13 @@
+package com.teknologiinformasi.restapi.apigeteway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApigetewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApigetewayApplication.class, args);
+	}
+
+}
---
+++ apigeteway/src/test/java/com/teknologiinformasi/restapi/apigeteway/ApigetewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teknologiinformasi.restapi.apigeteway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApigetewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
